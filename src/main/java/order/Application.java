411d1023b61d4405215a7a2e11f9073a6caf268a
김민수 MINSOO KIM <alstn113@gmail.com>
2024-08-18 22:08:29 +0900

    initial commit

---
+++ src/main/java/order/Application.java
@@ -0,0 +1,12 @@
+package order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        // TODO: 미션을 진행하세요.
+    }
+}
