369bcc87fd4dfc37a0c743dfed5d05e4437d1714
taek2222 <oot309@daum.net>
2024-10-14 00:28:57 +0900

    refactor: 배달비 상수명, 메서드 분리, 하드코딩 등 전체적으로 리팩토링

--- src/main/java/order/constant/DeliveryFee.java
+++ src/main/java/order/constant/DeliveryFee.java
@@ -1,22 +1,32 @@
 package order.constant;
 
 public enum DeliveryFee {
-    DELIVERY_FEE_LEVEL_1(2000),
-    DELIVERY_FEE_LEVEL_2(1000),
-    DELIVERY_FEE_LEVEL_3(0);
+    LEVEL_1(2000),
+    LEVEL_2(1000),
+    LEVEL_3(0);
 
-    private final int deliveryFee;
+    public static final int LEVEL_1_THRESHOLD = 50000;
+    public static final int LEVEL_2_THRESHOLD = 100000;
+    private final int fee;
 
     DeliveryFee(int deliveryFee) {
-        this.deliveryFee = deliveryFee;
+        this.fee = deliveryFee;
     }
 
-    public static int getDeliveryFee(int orderTotalPrice) {
-        if (orderTotalPrice < 50000)
-            return DELIVERY_FEE_LEVEL_1.deliveryFee;
-        if (orderTotalPrice < 100000)
-            return DELIVERY_FEE_LEVEL_2.deliveryFee;
+    public static int getDeliveryFee(int ordersPrice) {
+        if (isLEVEL1(ordersPrice))
+            return LEVEL_1.fee;
+        if (isLEVEL2(ordersPrice))
+            return LEVEL_2.fee;
 
-        return DELIVERY_FEE_LEVEL_3.deliveryFee;
+        return LEVEL_3.fee;
+    }
+
+    private static boolean isLEVEL1(int ordersPrice) {
+        return ordersPrice < LEVEL_1_THRESHOLD;
+    }
+
+    private static boolean isLEVEL2(int ordersPrice) {
+        return ordersPrice < LEVEL_2_THRESHOLD;
     }
 }
