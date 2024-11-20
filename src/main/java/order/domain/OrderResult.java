e2a2444b0d4351fb42933e2ab923a6c6c922fa63
taek2222 <oot309@daum.net>
2024-11-21 02:31:39 +0900

    feat(OrderResult): 배달비 측정 기능 구현 및 값 저장

--- src/main/java/order/domain/OrderResult.java
+++ src/main/java/order/domain/OrderResult.java
@@ -4,18 +4,34 @@ import static order.global.constant.ErrorMessage.INVALID_ORDER_MINIMUM_AMOUNT;
 
 public class OrderResult {
 
-    private static final int ORDER_MINIMUM_AMOUNT = 30000;
+    private static final int ORDER_MINIMUM_AMOUNT = 30_000;
 
     private final int amount;
+    private final int deliveryFee;
 
     public OrderResult(int amount) {
         validateAmount(amount);
         this.amount = amount;
+        this.deliveryFee = calculateDeliveryFee();
+    }
+
+    public int getDeliveryFee() {
+        return deliveryFee;
     }
 
     private void validateAmount(int amount) {
         if (amount < ORDER_MINIMUM_AMOUNT) {
             throw new IllegalArgumentException(INVALID_ORDER_MINIMUM_AMOUNT.get());
         }
+    }
+
+    private int calculateDeliveryFee() {
+        if (amount < 50_000) {
+            return 2_000;
+        }
+        if (amount < 100_000) {
+            return 1_000;
+        }
+        return 0;
     }
 }
