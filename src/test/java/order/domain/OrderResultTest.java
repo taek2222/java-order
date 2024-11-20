cbfeb1cafa1f8b0ef020622df0e6c3742e195c73
taek2222 <oot309@daum.net>
2024-11-21 02:24:51 +0900

    test(OrderResultTest): 합산 가격에 따라 배달비 측정 테스트

--- src/test/java/order/domain/OrderResultTest.java
+++ src/test/java/order/domain/OrderResultTest.java
@@ -1,12 +1,28 @@
 package order.domain;
 
 import static order.global.constant.ErrorMessage.INVALID_ORDER_MINIMUM_AMOUNT;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class OrderResultTest {
+
+    @ParameterizedTest(name = "테스트 금액: {arguments}")
+    @CsvSource({"35000, 2000", "75000, 1000", "100000, 0"})
+    void 합산_가격에_따라_배달비를_측정한다(int amount, int deliveryFee) {
+        // given
+        OrderResult orderResult = new OrderResult(amount);
+
+        // when
+        int result = orderResult.getDeliveryFee();
+
+        // then
+        assertThat(result)
+                .isEqualTo(deliveryFee);
+    }
 
     @ParameterizedTest(name = "테스트 금액: {arguments}")
     @ValueSource(ints = {-1_000, 0, 29_999})
