27de100d47bb8f345fa33298049fc4c004020fd6
taek2222 <oot309@daum.net>
2024-11-21 02:15:42 +0900

    test(OrderResultTest): 파라미터 검증 변경 및 테스트 간소화

--- src/test/java/order/domain/OrderResultTest.java
+++ src/test/java/order/domain/OrderResultTest.java
@@ -1,19 +1,17 @@
 package order.domain;
 
 import static order.global.constant.ErrorMessage.INVALID_ORDER_MINIMUM_AMOUNT;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class OrderResultTest {
 
-    @Test
-    void 최소_주문_금액에_미치지_못할_경우_예외가_발생한다() {
-        // given
-        int totalPrice = 20000;
-
-        // when & then
-        Assertions.assertThatThrownBy(() -> new OrderResult(totalPrice))
+    @ParameterizedTest(name = "테스트 금액: {arguments}")
+    @ValueSource(ints = {-1_000, 0, 29_999})
+    void 최소_주문_금액에_미치지_못할_경우_예외가_발생한다(int amount) {
+        assertThatThrownBy(() -> new OrderResult(amount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INVALID_ORDER_MINIMUM_AMOUNT.get());
     }
