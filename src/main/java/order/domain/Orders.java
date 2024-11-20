f3abf8d36f516f1d802491cabef51b10f1344417
taek2222 <oot309@daum.net>
2024-11-21 01:39:25 +0900

    feat(Orders): 주문 보관 객체 선언 및 검증
    - 메뉴가 오직 음료 및 최소 주문 수를 확인해 검증할 수 있도록 설계했습니다.

---
+++ src/main/java/order/domain/Orders.java
@@ -0,0 +1,15 @@
+package order.domain;
+
+import static order.global.validate.OrdersValidator.validateOrders;
+
+import java.util.List;
+
+public class Orders {
+
+    private final List<Order> orders;
+
+    public Orders(List<Order> orders) {
+        validateOrders(orders);
+        this.orders = orders;
+    }
+}
