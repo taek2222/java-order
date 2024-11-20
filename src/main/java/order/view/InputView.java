1246326d0de3fef443876bb8828f434cebc0d550
taek2222 <oot309@daum.net>
2024-11-20 14:01:01 +0900

    feat(InputView): 주문 안내 메시지 출력 기능 구현

---
+++ src/main/java/order/view/InputView.java
@@ -0,0 +1,13 @@
+package order.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String INPUT_ORDER_MENU_MESSAGE = "주문하실 메뉴와 수량을 입력해주세요. ex) 피자(2개), 감자튀김(1개), 콜라(3개)";
+
+    public String readOrderMenu() {
+        System.out.println(INPUT_ORDER_MENU_MESSAGE);
+        return SCANNER.nextLine();
+    }
+}
