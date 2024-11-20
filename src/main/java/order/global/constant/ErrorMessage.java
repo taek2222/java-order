c4fd9e066337c1cf0e8aabae22b2c4257d666317
taek2222 <oot309@daum.net>
2024-11-20 17:24:06 +0900

    feat(ErrorMessage): 에러 메시지 및 가변 인자 추가
    - 가변 인자 메서드를 추가해, 에러 메시지의 값을 추가할 수 있도록 변경했습니다.

--- src/main/java/order/global/constant/ErrorMessage.java
+++ src/main/java/order/global/constant/ErrorMessage.java
@@ -1,6 +1,7 @@
 package order.global.constant;
 
 public enum ErrorMessage {
+    INVALID_ORDER_QUANTITY("주문 메뉴의 수량은 최대 %d 까지만 가능 합니다."),
     INVALID_ORDER_FORMAT("주문 형식이 잘못되었습니다.");
 
     private static final String ERROR_PREFIX = "[ERROR] ";
@@ -13,5 +14,9 @@ public enum ErrorMessage {
 
     public String get() {
         return ERROR_PREFIX + message;
+    }
+
+    public String get(Object... value) {
+        return ERROR_PREFIX + String.format(message, value);
     }
 }
