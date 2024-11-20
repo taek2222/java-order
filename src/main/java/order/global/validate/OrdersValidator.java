8f22110011ca170ef93d64c46500b6c48cd528e6
taek2222 <oot309@daum.net>
2024-11-21 01:38:05 +0900

    feat(OrdersValidator): 주문 검증 클래스 정의
    - 최소 주문 수량을 체크하는 검증을 삽입했습니다.
    
    - 오직 메뉴가 음료인 경우를 체크하는 검증을 삽입했습니다.

---
+++ src/main/java/order/global/validate/OrdersValidator.java
@@ -0,0 +1,32 @@
+package order.global.validate;
+
+import static order.global.constant.ErrorMessage.INVALID_ORDERS_MINIMUM;
+import static order.global.constant.ErrorMessage.INVALID_ORDER_ONLY_DRINK;
+
+import java.util.List;
+import order.domain.Order;
+
+public class OrdersValidator {
+
+    private static final int ORDERS_MINIMUM = 1;
+
+    public static void validateOrders(List<Order> orders) {
+        validateOrdersMinimum(orders);
+        validateOnlyMenuTypeDrink(orders);
+    }
+
+    private static void validateOrdersMinimum(List<Order> orders) {
+        if (orders.size() < ORDERS_MINIMUM) {
+            throw new IllegalArgumentException(INVALID_ORDERS_MINIMUM.get());
+        }
+    }
+
+    private static void validateOnlyMenuTypeDrink(List<Order> orders) {
+        boolean hasNonDrink = orders.stream()
+                .anyMatch(order -> !order.isMenuTypeDrink());
+
+        if (!hasNonDrink) {
+            throw new IllegalArgumentException(INVALID_ORDER_ONLY_DRINK.get());
+        }
+    }
+}
