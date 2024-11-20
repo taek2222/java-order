18f2b4447f7af0c0d4625eca2843959a3ce94e84
taek2222 <oot309@daum.net>
2024-11-21 01:14:05 +0900

    test(OrdersTest): 최소 주문 갯수 예외 추가

--- src/main/java/order/global/constant/ErrorMessage.java
+++ src/main/java/order/global/constant/ErrorMessage.java
@@ -1,6 +1,8 @@
 package order.global.constant;
 
 public enum ErrorMessage {
+    INVALID_ORDER_ONLY_DRINK("음료만으로는 주문할 수 없습니다."),
+    INVALID_ORDERS_MINIMUM("주문 수는 최소 %d 이상이어야만 가능합니다."),
     INVALID_ORDER_QUANTITY("주문 메뉴의 수량은 최대 %d 까지만 가능 합니다."),
     INVALID_ORDER_FORMAT("주문 형식이 잘못되었습니다.");
 
--- src/test/java/order/domain/OrdersTest.java
+++ src/test/java/order/domain/OrdersTest.java
@@ -1,5 +1,10 @@
 package order.domain;
 
+import static order.domain.Menu.COLA;
+import static order.domain.Menu.PIZZA;
+import static order.global.constant.ErrorMessage.INVALID_ORDERS_MINIMUM;
+import static order.global.constant.ErrorMessage.INVALID_ORDER_ONLY_DRINK;
+
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,13 +12,40 @@ import org.junit.jupiter.api.Test;
 class OrdersTest {
 
     @Test
+    void 정상으로_주문할_경우_객체가_생성된다() {
+        // given
+        Order test1 = new Order(PIZZA, 1);
+        Order test2 = new Order(COLA, 1);
+        List<Order> orders = List.of(test1, test2);
+
+        // when
+        Orders result = new Orders(orders);
+
+        // then
+        Assertions.assertThat(result)
+                .isNotNull();
+    }
+
+    @Test
+    void 최소_주문_갯수가_아닌_경우_예외가_발생한다() {
+        // given
+        List<Order> orders = List.of();
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Orders(orders))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_ORDERS_MINIMUM.get());
+    }
+
+    @Test
     void 음료만_주문할_경우_예외가_발생한다() {
         // given
-        Order order = new Order(Menu.COLA, 1);
+        Order order = new Order(COLA, 1);
         List<Order> orders = List.of(order);
 
         // when & then
         Assertions.assertThatThrownBy(() -> new Orders(orders))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_ORDER_ONLY_DRINK.get());
     }
 }
