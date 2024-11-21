bfbc4eaf53c9c8177ab82072d80ab019f4f4ef7d
taek2222 <oot309@daum.net>
2024-11-21 18:04:52 +0900

    test(ServiceMenuTest): 테스트 케이스 추가

--- src/test/java/order/domain/ServiceMenuTest.java
+++ src/test/java/order/domain/ServiceMenuTest.java
@@ -1,22 +1,62 @@
 package order.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import order.domain.dto.ServiceResponse;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ServiceMenuTest {
 
     @Test
-    void 메인_수량_만큼_서비스를_추가한다() {
+    void 서비스_메뉴가_정상적으로_생성된다() {
         // given
-        int count = 5;
-        ServiceMenu orderService = new ServiceMenu(count);
+        int quantity = 5;
 
         // when
-        ServiceResponse response = orderService.createResponse();
+        ServiceMenu result = new ServiceMenu(quantity);
 
         // then
-        Assertions.assertThat(response.quantity())
-                .isEqualTo(count);
+        assertThat(result)
+                .isNotNull();
+    }
+
+    @Test
+    void 서비스_메뉴_응답을_정상적으로_생성한다() {
+        // given
+        int quantity = 5;
+        ServiceMenu serviceMenu = new ServiceMenu(quantity);
+
+        // when
+        ServiceResponse response = serviceMenu.createResponse();
+
+        // then
+        assertThat(response.menuName()).isEqualTo(Menu.DUMPLING);
+        assertThat(response.quantity()).isEqualTo(quantity);
+    }
+
+    @Test
+    void 서비스_수량이_있을_경우_true를_반환한다() {
+        // given
+        ServiceMenu serviceMenu = new ServiceMenu(5);
+
+        // when
+        boolean result = serviceMenu.hasServiceQuantity();
+
+        // then
+        assertThat(result)
+                .isTrue();
+    }
+
+    @Test
+    void 서비스_수량이_없을_경우_false를_반환한다() {
+        // given
+        ServiceMenu serviceMenu = new ServiceMenu(0);
+
+        // when
+        boolean result = serviceMenu.hasServiceQuantity();
+
+        // then
+        assertThat(result)
+                .isFalse();
     }
 }
